fe4738dcd9296c65a013775c62e346731c58edbd
Shikhar-ss <shikhar.sharma.ss.24@gmail.com>
2021-02-10 22:42:40 +0530

    added uncontroled input elements in the form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import './App.css';
+import React, {useState,useReducer} from 'react';
+
+const formDataReducer = (state,event) =>{
+  return {
+    ...state,
+    [event.name] : event.value
+  }
+}
 
 function App() {
+  const [ submitting, setSubmit] = useState(false);
+  const [formData, submitFormAction] = useReducer(formDataReducer,{})
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmit(true);
+    console.log("FORM_DATA_: ",formData)
+    setTimeout(()=>{
+      setSubmit(false)
+    }, 3000)
+  };
+
+  const handleChange = (e) => {
+    submitFormAction({
+      name : e.target.name,
+      value: e.target.value
+    })
+  }
+
   return (
-    <div className="App">
-      
+    <div className="wrapper">
+      <h1>APPLES</h1>
+      {submitting ? (<div>
+        Form is submitting...
+      </div>) : (<ul>
+        {Object.entries(formData).map(([name,value]) => (
+          <ul key={name}><strong>{name}</strong>: {value.toString()}</ul>
+        ))}
+      </ul>)}
+      <form >
+        <fieldset className="fieldset-wrapper">
+          <label>
+            <p>Name</p>
+            <input name="name" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>DOB</p>
+            <input name="DOB" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>City</p>
+            <input name="CITY" onChange={handleChange}/>
+          </label>
+        </fieldset>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </form>
     </div>
   );
 }
