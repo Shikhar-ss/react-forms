8d09f4b855ad49f3cff5401b428bd8a576db1927
Shikhar-ss <shikhar.sharma.ss.24@gmail.com>
2021-02-11 22:52:59 +0530

    updated form with controlled components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import './App.css';
 import React, {useState,useReducer} from 'react';
 
 const formDataReducer = (state,event) =>{
+  if(event.reset) {
+    return {
+      name: '',
+      DOB: '',
+      CITY: '',
+      apple: '',
+      count: '',
+      'gift-wrap': false
+    }
+  }
   return {
     ...state,
     [event.name] : event.value
@@ -17,6 +27,7 @@ function App() {
     setSubmit(true);
     console.log("FORM_DATA_: ",formData)
     setTimeout(()=>{
+      submitFormAction({reset:true})
       setSubmit(false)
     }, 3000)
   };
@@ -40,26 +51,26 @@ function App() {
         ))}
       </ul>)}
       <form >
-        <fieldset className="fieldset-wrapper">
+        <fieldset disabled={submitting} className="fieldset-wrapper">
           <label>
             <p>Name</p>
-            <input name="name" onChange={handleChange}/>
+            <input name="name" onChange={handleChange} value={formData.name || ''} />
           </label>
           <label>
             <p>DOB</p>
-            <input name="DOB" onChange={handleChange}/>
+            <input name="DOB" onChange={handleChange} value={formData.DOB || ''}/>
           </label>
           <label>
             <p>City</p>
-            <input name="CITY" onChange={handleChange}/>
+            <input name="CITY" onChange={handleChange} value={formData.CITY || ''}/>
           </label>
         </fieldset>
-        <fieldset>
+        <fieldset disabled={submitting}>
           <label>
             <p>
               Apples
             </p>
-            <select name="apple" onChange={handleChange}>
+            <select name="apple" onChange={handleChange} value={formData.apple || ''}>
               <option value="">--Please select apple--</option>
               <option value="garwhali">Gharwali</option>
               <option value="desi">Desi</option>
@@ -68,14 +79,14 @@ function App() {
           </label>
           <label>
            <p>Count</p>
-           <input type="number" name="count" onChange={handleChange} step="1"/>
+           <input type="number" name="count" onChange={handleChange} value={formData.count || ''} step="1"/>
          </label>
          <label>
            <p>Gift Wrap</p>
-           <input type="checkbox" name="gift-wrap" onChange={handleChange} />
+           <input type="checkbox" disabled={formData.apple !== 'desi'} name="gift-wrap" onChange={handleChange} checked={formData['gift-wrap'] || false}/>
          </label>
         </fieldset>
-        <button type="submit" onClick={handleSubmit}>Submit</button>
+        <button type="submit" disabled={submitting}onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
